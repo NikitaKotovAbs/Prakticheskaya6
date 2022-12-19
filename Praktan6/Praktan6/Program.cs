5c96f0233fe937a2633a3203533ba9aaff3dde93
NikitaKotovAbs <112644423+NikitaKotovAbs@users.noreply.github.com>
2022-12-19 19:05:35 +0300

    Add files via upload

---
+++ Praktan6/Praktan6/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
+using MyNamespace;
+using System.Security.Principal;
+
+namespace Praktan6
+{
+    public class Program
+    {
+        public static string FileName;
+        static void Main(string[] args)
+        {
+            Base();
+        }
+        public static void Base()
+        {
+            do
+            {
+                string wi = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Введите путь файла, который вы хотите октрыть");
+                Thread.Sleep(2000);
+                Console.SetCursorPosition(0, 1);
+                Console.WriteLine("----------------------------------------------   ");
+                string text = Console.ReadLine();
+                Console.SetCursorPosition(0, 2);
+                Console.WriteLine("                                                 ");
+                FileName = text;
+                string s = text;
+                s = s.Substring(s.Length - 2, 2);
+                Class1_output.deser = s;
+                if (System.IO.File.Exists(Convert.ToString(Path.GetFullPath(text))) == false)
+                {
+                    Console.SetCursorPosition(13, 1);
+                    Console.WriteLine("Не найден файл");
+                }
+                if (System.IO.File.Exists(Convert.ToString(Path.GetFullPath(text))) == true)
+                {
+                    Class1_output.Vivod();
+                }
+            } while (true);
+        }
+    }
+
+
+
+}
